eb428396dfb72a2ed98a4aad156947c6e6b0b6fe
Aniket05-2 <115588512+Aniket05-2@users.noreply.github.com>
2024-06-08 13:40:24 +0000

    Data Types

---
+++ Basics/datatypes.js
@@ -0,0 +1,13 @@
+//number => 2 to power 56
+// bigint
+//string=> ""
+//boolean=> true/false
+//null=>standalone value
+//undefined
+//symbol=>unique
+
+console.log(typeof null);
+//null
+
+console.log(typeof undefined)
+//undefined
