26b7e8a1224c2d95b75c60bc16f79cf0af7c6cdf
Aniket05-2 <115588512+Aniket05-2@users.noreply.github.com>
2024-06-08 13:38:52 +0000

    Conversion Operation

---
+++ Basics/conversionOperations.js
@@ -0,0 +1,18 @@
+let score ="33Abc"
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+//Nan
+
+// "33"=>33
+//"33abc"=>Nan
+//true =>1; false=>0
+
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
