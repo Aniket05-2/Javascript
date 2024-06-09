695f6fcb4ec958672227a298477ff03b81a103ed
Aniket05-2 <115588512+Aniket05-2@users.noreply.github.com>
2024-06-09 11:07:37 +0000

    Object destructuring

---
+++ Intermiadiate/objectpart3.js
@@ -0,0 +1,21 @@
+const course={
+    coursename:"js in hi9ndi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+
+
+
+const {courseInstructor:inst}=course
+console.log(inst)
+
+
+const navbar=({company})=>{
+
+}
+navbar(company="hitesh")
+
+navbar(company="hitesh")
+
+
+
