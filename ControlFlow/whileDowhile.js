e84aff7505cea1dc08466f3745bd2008eef446cc
Aniket05-2 <115588512+Aniket05-2@users.noreply.github.com>
2024-06-10 07:44:57 +0000

    WhileLoop

---
+++ ControlFlow/whileDowhile.js
@@ -0,0 +1,16 @@
+let index=0
+while(index <=10)
+    {
+        console.log(`Value of index is ${index}`)
+        index++
+    }
+
+
+
+   let i=0
+    do {
+        i++
+        console.log(i)
+        
+        
+    } while (i<10);
