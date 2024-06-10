edc378aa224ba44cbfbf4fc7e623a3a7d792f3cb
Aniket05-2 <115588512+Aniket05-2@users.noreply.github.com>
2024-06-10 07:45:31 +0000

    For Loop

---
+++ ControlFlow/forLoop.js
@@ -0,0 +1,13 @@
+const array=[10,20,30]
+
+
+for (let index = 0; index < array.length; index++) {
+
+    if(index==1)
+        {
+            console.log("Jai Hind")
+            break;
+        }
+    const element = array[index];
+    console.log(element)   
+}
